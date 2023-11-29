ba8fe47ce2108b482ed3e24048e37a28d9964daf
OnahNeLson <Onahnelson2@gmail.com>
2023-11-29 07:14:00 +0100

    island_perimeter

---
+++ 0x09-island_perimeter/4-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""prime Game"""
+
+def island_perimeter(grid: list) -> int:
+    """ Island Perimeter
+
+    Args:
+        grid (List[list]): island grid
+
+    Returns:
+        int: island perimeter
+    """
+    # initialize the perimeter
+    perimeter: int = 0
+    # loop through the grid
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            # check if its a land
+            if grid[i][j] == 1:
+                # calculate the perimeter which is sum of all 4 sides
+                # i.e 1+1+1+1 = 4
+                perimeter += 4
+                # check if the horizontal neighbour share sides
+                if i > 0 and grid[i-1][j] == 1:
+                    # they share sides, hence subtract 2 from the perimeter
+                    perimeter -= 2
+                # if the vertical neighbour share sides
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
