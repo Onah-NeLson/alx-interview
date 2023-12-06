43d8a2ae61e78d00cb14881e9340031f6ede9e9d
OnahNeLson <Onahnelson2@gmail.com>
2023-12-06 23:07:50 +0100

    Prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,31 +1,56 @@
 #!/usr/bin/python3
-"""Prime game winner determination"""
+
+""" Prime Game interview hands-0n """
+
+
+def is_prime(n):
+    """ Checks if a number given n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
+
+
+def calculate_primes(n, primes):
+    """ Calculate all primes """
+    top_prime = primes[-1]
+    if n > top_prime:
+        for i in range(top_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
+
 
 def isWinner(x, nums):
-    """Prime game winner determination"""
-    if x < 1 or not nums:
-        return None
+    """
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    """
 
-    m_wins = 0
-    b_wins = 0
+    players_wins = {"Maria": 0, "Ben": 0}
 
-    # generate a list of prime number based on the max numbers in num
-    n = max(nums)
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
+    primes = [0, 0, 2]
 
-    for x in range(2, int(n**0.5) + 1):
-        if primes[x]:
-            for y in range(x**2, n + 1, x):
-                primes[y] = False
+    calculate_primes(max(nums), primes)
 
-    # count the no of pm less than n i nums
-    for n in nums:
-        count = sum(primes[2:n+1])
-        b_wins += count % 2 == 0
-        m_wins += count % 2 == 1
+    for round in range(x):
+        sum_options = sum((i != 0 and i <= nums[round])
+                          for i in primes[:nums[round] + 1])
 
-    if m_wins == b_wins:
-        return None
+        if (sum_options % 2):
+            winner = "Maria"
+        else:
+            winner = "Ben"
 
-    return 'Maria' if m_wins > b_wins else 'Ben'
+        if winner:
+            players_wins[winner] += 1
+
+    if players_wins["Maria"] > players_wins["Ben"]:
+        return "Maria"
+    elif players_wins["Ben"] > players_wins["Maria"]:
+        return "Ben"
+
+    return None
